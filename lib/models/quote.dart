b407b6bffbab48b50acb80b4106dd6b5f27ad7dc
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-07-31 12:04:34 +0200

    Improved the styling of the app

--- lib/models/quote.dart
+++ lib/models/quote.dart
@@ -2,11 +2,15 @@ class Quote {
   final String id;
   final String text;
   final String author;
+  final String genre;
 
-  Quote({this.id, this.text, this.author});
+  Quote({this.id, this.text, this.author, this.genre});
 
   factory Quote.fromJson(Map<String, dynamic> json) {
     return Quote(
-        id: json['_id'], author: json['quoteAuthor'], text: json['quoteText']);
+        id: json['_id'],
+        author: json['quoteAuthor'],
+        text: json['quoteText'],
+        genre: json['quoteGenre']);
   }
 }
--- lib/screens/quotes_screen.dart
+++ lib/screens/quotes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quote_generator/models/quote.dart';
 import 'package:quote_generator/services/quoteApi.dart';
 
@@ -20,43 +21,106 @@ class _QuotesScreenState extends State<QuotesScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: FutureBuilder(
-          future: _quote,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            }
-
-            if (snapshot.hasError) {
-              return Text('ERROR');
-            }
-
-            return SingleChildScrollView(
-              padding: EdgeInsets.all(12.0),
-              child: Column(
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  RaisedButton(
-                    child: Text('Random'),
+                  Spacer(),
+                  FlatButton(
+                    child: Row(
+                      children: [
+                        Text('random'),
+                        SizedBox(width: 5.0),
+                        Icon(Icons.autorenew)
+                      ],
+                    ),
                     onPressed: () {
                       setState(() {
                         _quote = _api.randomQuote();
                       });
                     },
                   ),
-                  Text(
-                    snapshot.data.text,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 36,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
                 ],
               ),
-            );
-          },
+              Expanded(
+                child: Center(
+                  child: FutureBuilder(
+                    future: _quote,
+                    builder: (context, AsyncSnapshot<Quote> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      }
+
+                      if (snapshot.hasError) {
+                        return Text(
+                            'Error during quote fetching, try again later.');
+                      }
+
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '"${snapshot.data.text}"',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 36,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 80,
+                          ),
+                          FlatButton(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 30.0, vertical: 50.0),
+                            child: Row(
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      snapshot.data.author,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SizedBox(height: 8.0),
+                                    Text(
+                                      snapshot.data.genre,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 12,
+                                        color: Colors.grey,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Spacer(),
+                                Icon(Icons.arrow_forward)
+                              ],
+                            ),
+                            onPressed: () => {},
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Text(
+                'Kuroski @ DevChallendes.io',
+                style: GoogleFonts.montserrat(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey[400]),
+              ),
+            ],
+          ),
         ),
       ),
     );
